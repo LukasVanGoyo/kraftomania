1c56749a8d7d276f70a1d0b75f39c8c5e8e0d9a4
LukasVanGoyo <lukas.goinski@gmail.com>
2023-04-10 11:36:55 +0200

    changing settings file

--- website/settings.py
+++ website/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5zzut&7l3-((ne2adna+@b2skvw)hp5$)@j0djoe*zn8#1#yxl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
