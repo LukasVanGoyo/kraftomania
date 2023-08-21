89ce670dd5027a071e9804c72c33887717602bb0
LukasVanGoyo <lukas.goinski@gmail.com>
2023-08-21 13:05:07 +0200

    Updating settings file

--- app/settings.py
+++ app/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import environ
 from pathlib import Path
 import os
-
+import sys
 
 env = environ.Env()
 environ.Env.read_env()
@@ -28,9 +28,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+WSGI = 'django.core.wsgi' in sys.modules
+
+if WSGI == True:
+    DEBUG = False
+else:
+    DEBUG = True
+
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -133,6 +139,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
+
+
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
